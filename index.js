fa6d9dadd9c9b61167e35f0d379734b2a6d3a12d
xari <hrtanderson@gmail.com>
2022-06-16 17:23:33 +0200

    WIP: added webcomponents, refactored build chain

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ import prism_plugin from "markdown-it-prism";
 import webpack from "webpack";
 
 // This config object is used to compile the JS that may be included in individual blog posts
-const getWebpackConfig = (file, origin, destination) => ({
+const getWebpackConfig = (file, destination) => ({
   mode: "development",
-  entry: path.join(origin, file.name),
+  entry: file.path,
   resolve: {
     extensions: [".js"],
   },
@@ -46,86 +46,64 @@ const getWebpackConfig = (file, origin, destination) => ({
   },
 });
 
-function indexTemplate({ title, description, dateString: date, url }) {
-  return `
-  <article class="max-w-xl mb-10">
-      <header>
-          <h3 class="text-3xl">
-          <a href="./${url}">${title}</a>
-          </h3>
-          <small>${date}</small>
-      </header>
-      <section>
-          <p class="mt-0">${description}</p>
-      </section>
-  </article>`;
+function blogPreview({ title, description, date, path }) {
+  const link = `<a href=${path}>${title}</a>`;
+  const time = `<time datetime="${date}">${DateTime.fromISO(
+    date
+  ).toLocaleString(DateTime.DATE_FULL)}</time>`;
+
+  return `<blog-preview>
+<span slot="link">${link}</span>
+<span slot="date">${time}</span>
+<span slot="description">${description}</span>
+  </blog-preview>`;
 }
 
-function postTemplate({ title, description, dateString: date, content }) {
-  return `
-  <article>
-    <header class="my-3">
-      <h1 class="text-5xl">${title}</h1>
-      <p>${date}</p>
-    </header>
-    <section class="prose content">
-      ${content}
-    </section>
-  </article>
-`;
+function blogPost({ title, description, date, content }) {
+  const time = `<time datetime="${date}">${DateTime.fromISO(
+    date
+  ).toLocaleString(DateTime.DATE_FULL)}</time>`;
+
+  return `<blog-post>
+<span slot="title">${title}</span>
+<span slot="date">${time}</span>
+<span slot="content">${content}</span>
+  </blog-post>`;
 }
 
-function indexTemplateWrapper(html) {
-  return `
-   <header>
-    <h1 class="text-5xl my-3">
-      <a href="/">Ideas in Development</a>
-    </h1>
-   </header>
-   <main class="my-3">
-    ${html}
-   </main>`;
+function previewWrapper(content) {
+  return `<preview-wrapper>
+<span slot="content">${content}</span>
+  </preview-wrapper>`;
 }
 
-function postTemplateWrapper(html) {
-  return `
-  <header>
-    <h3 class="text-xl my-3">
-      <a href="/">Ideas in Development</a>
-    </h3>
-  </header>
-  <main class="my-3">
-    ${html}
-  </main>`;
+function blogPostWrapper(content) {
+  return `<blog-wrapper>
+<span slot="content">${content}</span>
+  </blog-wrapper>`;
 }
 
-function parseMD(md, callback = (x) => x) {
-  return new Promise((resolve) => {
-    let title, description, date; // frontmatter
+function parsePost(md) {
+  let title, description, date; // frontmatter
 
-    const content = MarkdownIt({
-      html: true,
-      linkify: true,
-      typographer: true,
-    })
-      .use(
-        front_matter_plugin,
-        (frontmatter) =>
-          ({ title, description, date } = YAML.parse(frontmatter))
-      )
-      .use(prism_plugin)
-      .render(md);
+  const content = MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+  })
+    .use(
+      front_matter_plugin,
+      (frontmatter) => ({ title, description, date } = YAML.parse(frontmatter))
+    )
+    .use(prism_plugin)
+    .render(md);
 
-    resolve(
-      callback({
-        title,
-        description,
-        date: new Date(date),
-        dateString: DateTime.fromISO(date).toLocaleString(DateTime.DATE_FULL),
-        content,
-      })
-    );
-  });
+  return {
+    title,
+    description,
+    date: new Date(date),
+    content,
+  };
 }
 
 function handleImg(origin, destination) {
@@ -137,96 +115,46 @@ function handleImg(origin, destination) {
     .toFile(destination);
 }
 
-function hydrateDOM(path, html) {
-  return JSDOM.fromFile(path).then((dom) => {
-    dom.window.document.getElementById("content").innerHTML = html;
-
-    return dom.serialize();
+// Resolves to an object, each of whose properties is an array of files
+async function getFileStructure(fileStructure, origin) {
+  return await fs.readdir(origin, { withFileTypes: true }).then((files) => {
+    return files.reduce(async (acc, file) => {
+      if (file.isDirectory() && file.name !== "node_modules") {
+        return getFileStructure(acc, path.join(origin, file.name));
+      } else {
+        return (await acc).hasOwnProperty(path.extname(file.name))
+          ? {
+              ...(await acc),
+              [path.extname(file.name)]: [
+                ...(await acc)[path.extname(file.name)],
+                {
+                  name: path.parse(file.name).name,
+                  path: path.relative(
+                    process.cwd(),
+                    path.join(origin, file.name)
+                  ),
+                },
+              ],
+            }
+          : acc;
+      }
+    }, fileStructure);
   });
 }
 
-// build() will return an array of Promises that are the result of recursively calling build.
-// These promises resolve to the post metadata (title, description, date) that will be used to build the index page.
-// The reason for all of this is that it lets me recursively build ./dist files for every directory in ./src....
-// Meaning that if I want to have content at xari.dev/horizontal-bar-plot/intro/index.html —I can do that!
-async function build(acc, origin, destination) {
-  return await fs.readdir(origin, { withFileTypes: true }).then((files) => {
-    mkdirSync(destination, { recursive: true });
+async function getFileContent(file) {
+  const blogContent = await fs
+    .readFile(file.path, "utf8")
+    .then(parsePost)
+    .then((postData) => ({
+      ...postData,
+      content: blogPostWrapper(blogPost(postData)),
+    }));
 
-    return files.reduce(async (acc, file) => {
-      if (file.isDirectory() && file.name !== "node_modules") {
-        return build(
-          acc,
-          path.join(origin, file.name),
-          path.join(destination, file.name)
-        );
-      } else {
-        switch (path.extname(file.name)) {
-          case ".png":
-          case ".jpg":
-          case ".jpeg":
-          case ".svg":
-          case ".gif":
-            handleImg(
-              path.join(origin, file.name),
-              path.join(destination, file.name)
-            );
-            return acc;
-            break;
-          case ".js":
-            const compiler = webpack(
-              getWebpackConfig(file, origin, destination)
-            );
-
-            console.info(`Compiling JS bundle for ${origin}.`);
-
-            compiler.run((err, stats) => {
-              if (err || stats.hasErrors()) {
-                console.log(err, stats);
-              }
-
-              compiler.close(console.error);
-            });
-            return acc;
-            break;
-          case ".md":
-            return [
-              ...(await acc), // Remember: we're in an async reducer
-              new Promise((resolve) => {
-                fs.readFile(path.join(origin, file.name), "utf8")
-                  .then((md) =>
-                    parseMD(md, (postData) => {
-                      resolve({
-                        ...postData,
-                        url: path.relative(
-                          path.join(__dirname, "dist"),
-                          destination
-                        ),
-                      });
-
-                      return postData; // Must return postData to not break the then() chain in parseMD()
-                    })
-                  )
-                  .then(postTemplate)
-                  .then((html) =>
-                    hydrateDOM(
-                      path.join(__dirname, "index.html"),
-                      postTemplateWrapper(html)
-                    )
-                  )
-                  .then((html) =>
-                    fs.writeFile(path.join(destination, "index.html"), html)
-                  );
-              }),
-            ];
-            break;
-          default:
-            console.info(`Unmatched file extension: ${file.name}.`);
-            return acc;
-        }
-      }
-    }, acc);
-  });
+  return {
+    ...file,
+    content: blogContent,
+  };
 }
 
 const __filename = fileURLToPath(import.meta.url);
@@ -235,33 +163,131 @@ const __dirname = dirname(__filename);
 const origin = path.join(__dirname, "src", "content");
 const destination = path.join(__dirname, "dist");
 
-const posts = await build([], origin, destination);
+const fileTypes = [".md", ".js", ".png", ".jpg", ".jpeg", ".svg", ".gif"];
 
-Promise.all(posts)
-  .then((x) => {
-    return x
-      .reduce((acc, cur) => {
-        const { content, ...rest } = cur; // Remove "content" property
+// Resolves to an object, each of whose properties is an array of files
+const { md, js, img } = await getFileStructure(
+  fileTypes.reduce((o, key) => ({ ...o, [key]: [] }), {}),
+  origin
+).then(({ ".md": md, ".js": js, ...img }) => {
+  const _md = md.map(getFileContent);
+  const _js = js.map((file) => webpack(getWebpackConfig(file, destination)));
+  const _img = Object.keys(img)
+    .reduce(function (paths, key) {
+      return paths.concat(img[key]);
+    }, [])
+    .map(({ path }) => {
+      return sharp(path).resize({
+        width: 600,
+        fit: "inside",
+      });
+    });
 
-        !Object.values(rest).includes(undefined) ? rest : null; // Remove whole object if any fields are undefined
-
-        return [...acc, rest];
-      }, [])
-      .sort((a, b) => b.date - a.date);
-  })
-  .then((x) =>
-    x.reduce((acc, cur) => {
-      return acc.concat(indexTemplate(cur));
-    }, "")
-  )
-  .then((x) =>
-    hydrateDOM(path.join(__dirname, "index.html"), indexTemplateWrapper(x))
-  )
-  .then((html) => fs.writeFile(path.join(destination, "index.html"), html));
-
-const app = express();
-const port = 3000;
-
-app.use(express.static(path.join(__dirname, "dist"))).listen(port, () => {
-  console.log(`Blog app listening on port ${port}`);
+  return {
+    md: _md,
+    js: _js,
+    img: _img,
+  };
 });
+
+// Create the blog posts
+const posts = await Promise.all(md)
+  .then((x) =>
+    x.map(({ content: blogPost, ...postData }) => ({
+      ...postData,
+      title: blogPost.title,
+      date: blogPost.date,
+      description: blogPost.description,
+      content: JSDOM.fromFile(path.join(__dirname, "index.html")).then(
+        (dom) => {
+          dom.window.document.getElementById("content").innerHTML =
+            blogPost.content;
+
+          return dom.serialize();
+        }
+      ),
+    }))
+  )
+  .then((x) =>
+    x.map(async ({ name, path: src_path, content, ...details }) => {
+      const destination_path = path.join(
+        destination,
+        path.relative(origin, path.parse(src_path).dir)
+      );
+
+      mkdirSync(destination_path, { recursive: true });
+      fs.writeFile(`${path.join(destination_path, name)}.html`, await content);
+
+      return { path: path.relative(destination, destination_path), ...details };
+    })
+  );
+
+// Create the front-page
+Promise.all(posts)
+  .then((x) =>
+    JSDOM.fromFile(path.join(__dirname, "index.html")).then((dom) => {
+      dom.window.document.getElementById("content").innerHTML = previewWrapper(
+        x.map(blogPreview)
+      );
+
+      return dom.serialize();
+    })
+  )
+  .then(async (html) =>
+    fs.writeFile(path.join(destination, "index.html"), await html)
+  );
+
+// Compile the JS
+// js.forEach((compiler) =>
+//   compiler.run((err, stats) => {
+//     if (err || stats.hasErrors()) {
+//       console.log(err, stats);
+//     }
+//
+//     compiler.close(console.error);
+//   })
+// );
+
+// Write the images
+// img.forEach((x) => x.toFile(destination));
+
+// Promise.all(files[".md"]).then(console.log);
+
+// dateString: DateTime.fromISO(date).toLocaleString(DateTime.DATE_FULL),
+
+// const withContent = files.map(getFileContent);
+
+// files.Promise.all(files)
+//   .then((obj) => {
+//     fs.writeFile(
+//       `${path.join(destination, path.parse(file.name).name)}.json`,
+//       JSON.stringify(obj)
+//     );
+//
+//     return obj;
+//   })
+//   .then((x) => {
+//     return x
+//       .reduce((acc, cur) => {
+//         const { content, ...rest } = cur; // Remove "content" property
+//
+//         !Object.values(rest).includes(undefined) ? rest : null; // Remove whole object if any fields are undefined
+//
+//         return [...acc, rest];
+//       }, [])
+//       .sort((a, b) => b.date - a.date);
+//   })
+//   .then((x) =>
+//     x.reduce((acc, cur) => {
+//       return acc.concat(blogPreview(cur));
+//     }, "")
+//   )
+//   .then(previewWrapper)
+//   .then((html) =>
+//     JSDOM.fromFile(path.join(__dirname, "index.html")).then((dom) => {
+//       dom.window.document.getElementById("content").innerHTML = html;
+//
+//       return dom.serialize();
+//     })
+//   )
+//   .then((html) => fs.writeFile(path.join(destination, "index.html"), html));
